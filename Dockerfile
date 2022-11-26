06044abc008b068544263a731ae63a83e679c132
Julian Haupt <julian@haupt.media>
2022-11-26 15:23:14 +0100

    add hazelcast exporter jar in docker image

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,19 @@ ARG ZEEBE_VERSION=8.1.4
 FROM alpine:latest as unzipper
 
 ARG ZEEBE_KAFKA_EXPORTER_VERSION=3.1.1
+ARG ZEEBE_HAZELCAST_EXPORTER_VERSION=1.2.1
 
-RUN apk add unzip wget curl
+RUN apk add unzip curl
+
+# download & unzip kafka exporter
 RUN curl -L https://github.com/camunda-community-hub/zeebe-kafka-exporter/releases/download/${ZEEBE_KAFKA_EXPORTER_VERSION}/zeebe-kafka-exporter-${ZEEBE_KAFKA_EXPORTER_VERSION}.zip -o /tmp/zeebe-kafka-exporter.zip && \
     unzip /tmp/zeebe-kafka-exporter.zip -d /tmp && \
     mv /tmp/target/nexus-staging/deferred/io/zeebe/zeebe-kafka-exporter/${ZEEBE_KAFKA_EXPORTER_VERSION}/zeebe-kafka-exporter-${ZEEBE_KAFKA_EXPORTER_VERSION}-jar-with-dependencies.jar /opt/zeebe-kafka-exporter.jar
 
+# download hazelcast exporter
+RUN curl -L https://github.com/camunda-community-hub/zeebe-hazelcast-exporter/releases/download/${ZEEBE_HAZELCAST_EXPORTER_VERSION}/zeebe-hazelcast-exporter-${ZEEBE_HAZELCAST_EXPORTER_VERSION}-jar-with-dependencies.jar -o /opt/zeebe-hazelcast-exporter.jar
+
 FROM camunda/zeebe:${ZEEBE_VERSION}
 
 ADD kafka/kafka-exporter.yml /usr/local/zeebe/config/kafka-exporter.yml
-COPY --from=unzipper /opt/zeebe-kafka-exporter.jar /usr/local/zeebe/lib/
+COPY --from=unzipper /opt/*.jar /usr/local/zeebe/lib/
--- build_latest_version.sh
+++ build_latest_version.sh
@@ -3,15 +3,18 @@
 DOCKER_NAME=hauptmedia/zeebe-with-exporters
 
 # this needs the github cli from https://cli.github.com/
-ZEBEE_VERSION=$(gh release list --exclude-drafts --limit 1 --repo camunda/zeebe | cut -f1)
-ZEBEE_KAFKA_EXPORTER_VERSION=$(gh release list --exclude-drafts --limit 1 --repo camunda-community-hub/zeebe-kafka-exporter | cut -f1)
+ZEBEE_VERSION=$(gh release list --exclude-drafts --limit 1 --repo camunda/zeebe | cut -f3)
+ZEBEE_KAFKA_EXPORTER_VERSION=$(gh release list --exclude-drafts --limit 1 --repo camunda-community-hub/zeebe-kafka-exporter | cut -f3)
+ZEBEE_HAZELCAST_EXPORTER_VERSION=$(gh release list --exclude-drafts --limit 1 --repo camunda-community-hub/zeebe-hazelcast-exporter | cut -f3)
 
-echo "Latest Zeebe Release Version: ${ZEBEE_VERSION}"
-echo "Latest Zeebe Kafka Exporter Version: ${ZEBEE_KAFKA_EXPORTER_VERSION}"
+echo "Latest Zeebe Version: ${ZEBEE_VERSION}"
+echo "Latest Kafka Exporter Version: ${ZEBEE_KAFKA_EXPORTER_VERSION}"
+echo "Latest Hazelcast Exporter Version: ${ZEBEE_HAZELCAST_EXPORTER_VERSION}"
 
 docker build -t ${DOCKER_NAME}:${ZEBEE_VERSION} \
 --build-arg ZEEBE_VERSION=${ZEBEE_VERSION} \
 --build-arg ZEEBE_KAFKA_EXPORTER_VERSION=${ZEBEE_KAFKA_EXPORTER_VERSION} \
+--build-arg ZEEBE_HAZELCAST_EXPORTER_VERSION=${ZEBEE_HAZELCAST_EXPORTER_VERSION} \
 .
 
 docker tag ${DOCKER_NAME}:${ZEBEE_VERSION} ${DOCKER_NAME}:latest
